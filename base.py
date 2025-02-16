4989dbfeb9fc2db73cf729b0879e93d7bcd5d637
Gullwinkle <gullwinkle@gmail.com>
2025-02-16 22:11:42 +0500

    dishes from DB

--- base.py
+++ base.py
@@ -23,3 +23,11 @@ def get_categories(restaurant_id):
     result = cursor.fetchall()
     conn.close()
     return [{"id": row[0], "name": row[1]} for row in result]
+
+def get_dishes(category_id):
+    conn = sqlite3.connect("db/food_orders")
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name, price, description FROM dishes WHERE category_id = ?", (category_id,))
+    result = cursor.fetchall()
+    conn.close()
+    return [{"id": row[0], "name": row[1], "price": row[2], "description": row[3]} for row in result]
